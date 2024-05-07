3fbc846458719cecdb4049ff047a1293404753c2
Ivy Kutswa <kutswaivy@gmail.com>
2024-05-07 10:54:58 -0700

    added files

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,37 @@
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Replace Nginx default configuration
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  source  => 'puppet:///modules/nginx/nginx.conf',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Create a custom Nginx configuration file
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => file,
+  content => 'server {
+                listen 80 default_server;
+                listen [::]:80 default_server;
+                server_name _;
+                location / {
+                  add_header X-Served-By $hostname;
+                  root /var/www/html;
+                  index index.html;
+                }
+              }',
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Restart Nginx service when configuration changes
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/sites-enabled/default'],
+}
